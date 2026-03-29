94c40fc45dcd664fb935b2bce12b456b7de4782a
Eliasu <elias@commandg.de>
2026-03-29 01:09:28 +0100

    adding git credentials

--- config/statamic/git.php
+++ config/statamic/git.php
@@ -78,8 +78,8 @@ return [
     'use_authenticated' => true,
 
     'user' => [
-        'name' => env('STATAMIC_GIT_USER_NAME', 'Spock'),
-        'email' => env('STATAMIC_GIT_USER_EMAIL', 'spock@example.com'),
+        'name' => env('STATAMIC_GIT_USER_NAME', 'Elisu'),
+        'email' => env('STATAMIC_GIT_USER_EMAIL', 'elias@commandg.de'),
     ],
 
     /*
